1f0c50d5da1c3da188947c69d488a5179eda0521
iZeaoGamer <jameshayward14@icloud.com>
2020-04-29 15:17:30 +0100

    Update Main.php

--- src/ItsGabry/NanoWild/Main.php
+++ src/ItsGabry/NanoWild/Main.php
@@ -20,18 +20,14 @@ class Main extends PluginBase implements Listener {
         switch ($command->getName()) {
             case "wild":
                 if($sender instanceof Player) {
-                    if (in_array($sender->getLevel()->getFolderName(), $this->getConfig()->get("worlds"))) {
                         $range = $this->getConfig()->get("range");
                         $x = rand(-$range, $range);
                         $z = rand(-$range, $range);
                         $y = 256;
                         $sender->getLevel()->populateChunk($x >> 4, $z >> 4, true);
                         $sender->sendMessage(TextFormat::GREEN . "Wait 10 seconds...");
-                        $this->getScheduler()->scheduleDelayedTask(new NanoWildTask($sender, new Position($x, $y, $z)), 200);
+                        $this->getScheduler()->scheduleDelayedTask(new NanoWildTask($sender, new Position($x, $y, $z, $this->getServer()->getLevelByName("wild"))), 200);
                         $this->getScheduler()->scheduleRepeatingTask(new TitleTask($sender, $this), 20);
-                    }else{
-                        $sender->sendMessage(TextFormat::RED . "You cannot do this command here!");
-                    }
                 }
         }
       return true;
